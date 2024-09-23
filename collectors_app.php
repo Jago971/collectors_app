22584147a34227e637800071e42fb0576854c0d2
Matt_Mannings <matt.mannings97@gmail.com>
2024-09-23 09:39:19 +0100

    first commit, file and repo setup

---
+++ collectors_app.php
@@ -0,0 +1,25 @@
+<?php
+// story one:
+// DB to store items
+// items must have at least 3 stats
+// display DB items
+
+// story two:
+// add new items
+
+
+// story 1 tasks:
+
+// git repo
+// git connect
+// git main
+// git branch story 1 (S1)
+// git branch story 1 task 1 (S1T1)
+// create DB with 3 items, 3 stats each
+// fetch db and display
+
+// story 2 tasks:
+
+// html form
+// $_GET form data
+// move data to DB
