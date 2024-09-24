da89bc3021fc9e7f58906f262514d8520092e531
Matt_Mannings <matt.mannings97@gmail.com>
2024-09-24 15:11:38 +0100

    changes to index.php

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <?php
-// story one:
-// DB to store items
-// items must have at least 3 stats
-// display DB items
-
-// story two:
-// add new items
-
-
-// story 1 tasks:
-
-// git repo ✅
-// git connect ✅
-// git main ✅
-// git branch story 1 (S1) ✅
-// create DB with 3 items, 3 stats each ✅
-// fetch db and display ✅
-
-// story 2 tasks:
-
-// html form ✅
-// $_GET form data ✅
-// move data to DB ✅
-
-// trainer tasks:
-
-// validation of inputs:
-// DB table for sizes, color, pattern ✅
-// dropdowns that populate colors, sizes, patterns ✅
-
-//----------------------------------------------------------------------------------------------------------------------required files
 require_once 'src/functions.php';
 ?>
 
@@ -78,18 +47,6 @@ require_once 'src/functions.php';
     <?php
     echo displaySocksCollection(connectDB());
     ?>
-    <div class="sock-BG">
-        <div class="sock-container purple XS">
-            <div class="sock-ankle">
-                <div class="cuff detail"></div>
-                <div class="heel"></div>
-            </div>
-            <div class="sock-foot">
-                <div class="toe"></div>
-            </div>
-        </div>
-    </div>
-
 </div>
 </body>
 </html>
