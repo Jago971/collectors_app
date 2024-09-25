eb56e4a89667c14c4d974ad30a14dff2f4b5f7f5
Matt_Mannings <matt.mannings97@gmail.com>
2024-09-25 10:01:09 +0100

    missed responsive tag in head

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ require_once 'src/functions.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <title>collectors_app</title>
     <style>
